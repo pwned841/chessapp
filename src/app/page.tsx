f61734e3d8cab5c8df45de7d192588ece511a93f
pwned841 <remiemeriaupro@gmail.com>
2024-12-16 15:27:40 +0100

    Creating card in home page + forcing dark mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import Form from "@/components/Form";
+import {CardHome} from "@/components/Card";
 
 export default function Home() {
   return (
       <>
-        <Form></Form>
+          <div className="flex justify-center mt-24">
+            <CardHome></CardHome>
+          </div>
       </>
   );
 }
