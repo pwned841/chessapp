a64fe97b1986c15065b8633cfbe21e6000dec22e
pwned841 <remiemeriaupro@gmail.com>
2024-12-19 11:12:53 +0100

    Build Vercel version

--- src/app/api/players/[id]/route.ts
+++ src/app/api/players/[id]/route.ts
@@ -1,46 +1,34 @@
-// app/api/players/[id]/route.ts
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-interface Props {
-    params: {
-        id: string;
-    };
-}
 
-export async function GET(request: NextRequest, { params }: Props): Promise<NextResponse> {
+export async function GET(
+    request: NextRequest,
+    { params }: {params: Promise<{
+        id: string;
+    }>}
+): Promise<NextResponse> {
     try {
-        const playerId = parseInt(params.id, 10);
+        const id = (await params).id;
+        const playerId = parseInt(id, 10);
 
         if (isNaN(playerId)) {
-            return NextResponse.json(
-                { error: 'Invalid player ID' },
-                { status: 400 }
-            );
+            return NextResponse.json({ error: 'Invalid player ID' }, { status: 400 });
         }
 
         const player = await prisma.player.findUnique({
-            where: {
-                fideid: playerId
-            }
+            where: { fideid: playerId },
         });
 
         if (!player) {
-            return NextResponse.json(
-                { error: 'Player not found' },
-                { status: 404 }
-            );
+            return NextResponse.json({ error: 'Player not found' }, { status: 404 });
         }
 
-        return NextResponse.json(player);
+        return NextResponse.json(player, { status: 200 });
     } catch (error) {
-        console.error('Error fetching player:', error);
-        return NextResponse.json(
-            { error: 'Internal Server Error' },
-            { status: 500 }
-        );
+        console.error('Error fetching player:', error.message, error.stack);
+        return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
     }
 }
